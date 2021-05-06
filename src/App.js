41c42590aa0e66bc3464b4410fd8edb185eff428
Bessong lawrence <orockbessonglawrence@gmail.com>
2021-05-06 15:13:20 +0100

    Building the form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+// const  App = () => {
+
+//   const [count, setCount] = useState(0);
+
+//   return (
+  
+//     <div className="App">
+//         <div> 
+//           <h1> Count : { count } </h1>
+//           <button onClick={() => setCount(count+1)}>+1</button>
+//           <button onClick={() => setCount(count-1)}>-1</button>
+//           <button onClick={() => setCount(0)}>Reset</button>
+//         </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const details = {
+  title : "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
+}
+
+const App = () => {
+  return(
+    <div>
+      <h1>{details.title}</h1>
+      {details.subtitle && <p>{details.subtitle}</p>}
+      <p>{ details.options.length > 0 ? "You have options" : "No options" }</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
     </div>
   );
 }
